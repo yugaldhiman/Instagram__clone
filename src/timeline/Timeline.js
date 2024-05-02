b9adfe24d01a84281fa5589d8b2a8f958a63c5c2
yugal dhiman <yugaldhiman14@gmail.com>
2024-05-03 00:14:15 +0530

    add postIcons

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Post from "./posts/Post";
 import Sugesstions from "../sugesstionss/Sugesstions";
 import "./Timeline.css";
 
 function Timeline() {
   return (
     <div className="timeline">
-      <div className="timeline__left">Timeline</div>
+      <div className="timeline__left">
+        <div className="timeline__posts">
+          <Post />
+        </div>
+      </div>
       <div className="timeline__right">
         <Sugesstions />
       </div>
