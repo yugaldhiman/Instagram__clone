99d794aff99e3f04a14a21723cc8d5b91312ad85
yugal dhiman <yugaldhiman14@gmail.com>
2024-05-03 01:32:52 +0530

    add usestate and loops

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./posts/Post";
 import Sugesstions from "../sugesstionss/Sugesstions";
 import "./Timeline.css";
 
 function Timeline() {
+  const [posts, setPosts] = useState([
+    {
+      user: "rajan dhiman_",
+      postImage:
+        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      likes: 180,
+      timestamp: "12h",
+    },
+    {
+      user: "tomar_23",
+      postImage:
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg",
+      likes: 240,
+      timestamp: "5h",
+    },
+    {
+      user: "himanshu dhiman_",
+      postImage:
+        "https://plus.unsplash.com/premium_photo-1687382111414-7b87afa5da34?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGltYWdlfGVufDB8fDB8fHww",
+      likes: 160,
+      timestamp: "10h",
+    },
+    {
+      user: "yugal dhiman_",
+      postImage:
+        "https://images.unsplash.com/photo-1613323593608-abc90fec84ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
+      likes: 190,
+      timestamp: "10h",
+    },
+  ]);
   return (
     <div className="timeline">
       <div className="timeline__left">
         <div className="timeline__posts">
-          <Post />
+          {posts.map((post) => (
+            <Post
+              user={post.user}
+              postImage={post.postImage}
+              like={post.likes}
+              timestamp={post.timestamp}
+            />
+          ))}
         </div>
       </div>
       <div className="timeline__right">
--- src/timeline/posts/Post.js
+++ src/timeline/posts/Post.js
@@ -6,22 +6,19 @@ import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
-function Post() {
+function Post({ user, postImage, like, timestamp }) {
   return (
     <div className="post">
       <div className="post__header">
         <div className="post__headerAuthor">
-          <Avatar>R</Avatar>
-          rajan_ . <span>12h</span>
+          <Avatar>{user.charAt(0).toUpperCase()}</Avatar>
+          {user} . <span>{timestamp}</span>
         </div>
         <MoreHorizIcon />
       </div>
 
       <div className="post__image">
-        <img
-          src="https://images.unsplash.com/photo-1613323593608-abc90fec84ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
-          alt=""
-        />
+        <img src={postImage} alt="error" />
       </div>
       <div className="post__footer">
         <div className="post__footerIcons">
@@ -34,63 +31,7 @@ function Post() {
             <BookmarkBorderIcon className="postIcon" />
           </div>
         </div>
-        Liked by 180 people.
-      </div>
-      <br />
-      <br />
-      <div className="post__header">
-        <div className="post__headerAuthor">
-          <Avatar>T</Avatar>
-          tomar_ . <span>5h</span>
-        </div>
-        <MoreHorizIcon />
-      </div>
-      <div className="post__image">
-        <img
-          src="https://images.unsplash.com/photo-1613323593608-abc90fec84ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
-          alt=""
-        />
-      </div>
-      <div className="post__footer">
-        <div className="post__footerIcons">
-          <div className="post__iconsMain">
-            <FavoriteBorder className="postIcon" />
-            <ChatBubbleOutlineIcon className="postIcon" />
-            <TelegramIcon className="postIcon" />
-          </div>
-          <div className="post__iconsSave">
-            <BookmarkBorderIcon className="postIcon" />
-          </div>
-        </div>
-        Liked by 240 people.
-      </div>
-      <br />
-      <br />
-      <div className="post__header">
-        <div className="post__headerAuthor">
-          <Avatar>H</Avatar>
-          himanshu_ . <span>10h</span>
-        </div>
-        <MoreHorizIcon />
-      </div>
-      <div className="post__image">
-        <img
-          src="https://images.unsplash.com/photo-1613323593608-abc90fec84ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
-          alt=""
-        />
-      </div>
-      <div className="post__footer">
-        <div className="post__footerIcons">
-          <div className="post__iconsMain">
-            <FavoriteBorder className="postIcon" />
-            <ChatBubbleOutlineIcon className="postIcon" />
-            <TelegramIcon className="postIcon" />
-          </div>
-          <div className="post__iconsSave">
-            <BookmarkBorderIcon className="postIcon" />
-          </div>
-        </div>
-        Liked by 160 people.
+        Liked by {like} people.
       </div>
     </div>
   );
